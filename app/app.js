33c09dd38e9bd106f7efc0a3e0ee7e66d05ccd2b
webtoast <webtoast@gmail.com>
2016-08-14 11:55:38 -0500

    move renderer code into app.js
    * drag and drop functionality
    * class to test log statements of file/directory paths

--- app/app.js
+++ app/app.js
@@ -20,3 +20,77 @@ console.log('The author of this app is:', appDir.read('package.json', 'json').au
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('greet').innerHTML = greet();
 });
+
+document.ondragover = document.ondrop = (ev) => {
+    ev.preventDefault();
+}
+
+document.ondrop = (ev) => {
+    ev.preventDefault();
+    console.log('Files Dropped!!');
+    let FileReportView = new FileReport(ev);
+}
+
+class FileReport {
+    constructor(e) {
+        this.fileList = e.dataTransfer.files;
+        this.savePath = new String();
+        this.arrayofFiles = new Array();
+
+        // kick off the fun
+        this.init();
+    }
+
+    init() {
+        // for (let f of unoptimizedFiles) {
+        //   console.log('File(s) you dragged here: ', f.path)
+        // }
+
+        // just grab the fist element in the array
+        // to determine the path to save to
+        this.getSavePath(this.fileList[0].path);
+        this.getFilesArray(this.fileList[0].path);
+
+        //console.log(this.fileList[0].path.indexOf('.'));
+
+        //this.optimizeImages();
+
+        //console.log(this.fileList);
+        // console.log(new Array());
+    }
+
+    getSavePath(fullPath) {
+        this.pathArray = fullPath.split('/');
+        this.pathArray.pop();
+        this.savePath = this.pathArray.join('/');
+        this.savePath += '/chef/';
+        console.log("The path to save to = " + this.savePath);
+    }
+
+    getFilesArray(fullPath) {
+        var tempStr = new String();
+        var i = new Number(0);
+
+        for (let f of this.fileList) {
+            this.arrayofFiles.push(f.path);
+        }
+        if (fullPath.indexOf('.') === -1) {
+            // update to use a globbing pattern
+            tempStr = this.arrayofFiles[0];
+            this.arrayofFiles[0] = tempStr + '/*.{jpg,png}';
+            // update the savePath variable to organize them in the same folder structure
+        }
+
+        console.log(this.arrayofFiles);
+    }
+
+    optimizeImages() {
+        imagemin(this.arrayofFiles, this.savePath, {
+            plugins: [
+                imageminPngquant({quality: '65-80'})
+            ]
+        }).then(files => {
+            console.log(files);
+        });
+    }
+};
