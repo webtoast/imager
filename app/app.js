b07ee30188770134d1e9a5077625abb44bebe144
webtoast <webtoast@gmail.com>
2016-11-07 23:00:30 -0600

    move class into its own file

--- app/app.js
+++ app/app.js
@@ -7,9 +7,7 @@ import { remote } from 'electron'; // native electron module
 import jetpack from 'fs-jetpack'; // module loaded from npm
 import { greet } from './hello_world/hello_world'; // code authored by you in this project
 import env from './env';
-
-import imagemin from 'imagemin';
-import imageminPngquant from 'imagemin-pngquant';
+import Optimizer from './modules/optimizer';
 
 console.log('Loaded environment variables:', env);
 
@@ -31,72 +29,5 @@ document.ondragover = document.ondrop = (ev) => {
 document.ondrop = (ev) => {
     ev.preventDefault();
     console.log('Files Dropped!!');
-    let FileReportView = new FileReport(ev);
+    let FileReportView = new Optimizer(ev);
 }
-
-class FileReport {
-    constructor(e) {
-        this.fileList = e.dataTransfer.files;
-        this.savePath = new String();
-        this.arrayofFiles = new Array();
-        this.saveFolder = '/OPTIMIZED/';
-
-        // kick off the fun
-        this.init();
-    }
-
-    init() {
-        // for (let f of unoptimizedFiles) {
-        //   console.log('File(s) you dragged here: ', f.path)
-        // }
-
-        // just grab the fist element in the array
-        // to determine the path to save to
-        this.getSavePath(this.fileList[0].path);
-        this.getFilesArray(this.fileList[0].path);
-
-        //console.log(this.fileList[0].path.indexOf('.'));
-
-        this.optimizeImages();
-
-        //console.log(this.fileList);
-        // console.log(new Array());
-    }
-
-    getSavePath(fullPath) {
-        var tempPath = new String();
-        this.pathArray = fullPath.split('/');
-        //remove the last element which is the file
-        this.pathArray.pop();
-        tempPath = this.pathArray.join('/');
-        this.savePath = tempPath + this.saveFolder;
-        console.log("The path to save to = " + this.savePath);
-    }
-
-    getFilesArray(fullPath) {
-        var tempStr = new String();
-        var i = new Number(0);
-
-        for (let f of this.fileList) {
-            this.arrayofFiles.push(f.path);
-        }
-        if (fullPath.indexOf('.') === -1) {
-            // update to use a globbing pattern
-            tempStr = this.arrayofFiles[0];
-            this.arrayofFiles[0] = tempStr + '/**/*.{jpg,png}';
-            // update the savePath variable to organize them in the same folder structure
-        }
-
-        console.log(this.arrayofFiles);
-    }
-
-    optimizeImages() {
-        imagemin(this.arrayofFiles, this.savePath, {
-            plugins: [
-                imageminPngquant({quality: '65-80'})
-            ]
-        }).then(files => {
-            console.log(files);
-        });
-    }
-};
---
+++ app/modules/optimizer.js
@@ -0,0 +1,69 @@
+import imagemin from 'imagemin';
+import imageminPngquant from 'imagemin-pngquant';
+
+export default class Optimizer {
+    constructor(e) {
+        this.fileList = e.dataTransfer.files;
+        this.savePath = new String();
+        this.arrayofFiles = new Array();
+        this.saveFolder = '/OPTIMIZED/';
+
+        // kick off the fun
+        this.init();
+    }
+
+    init() {
+        // for (let f of unoptimizedFiles) {
+        //   console.log('File(s) you dragged here: ', f.path)
+        // }
+
+        // just grab the fist element in the array
+        // to determine the path to save to
+        this.getSavePath(this.fileList[0].path);
+        this.getFilesArray(this.fileList[0].path);
+
+        //console.log(this.fileList[0].path.indexOf('.'));
+
+        this.optimizeImages();
+
+        //console.log(this.fileList);
+        // console.log(new Array());
+    }
+
+    getSavePath(fullPath) {
+        var tempPath = new String();
+        this.pathArray = fullPath.split('/');
+        //remove the last element which is the file
+        this.pathArray.pop();
+        tempPath = this.pathArray.join('/');
+        this.savePath = tempPath + this.saveFolder;
+        console.log("The path to save to = " + this.savePath);
+    }
+
+    getFilesArray(fullPath) {
+        var tempStr = new String();
+        var i = new Number(0);
+
+        for (let f of this.fileList) {
+            this.arrayofFiles.push(f.path);
+        }
+        if (fullPath.indexOf('.') === -1) {
+            // update to use a globbing pattern
+            tempStr = this.arrayofFiles[0];
+            this.arrayofFiles[0] = tempStr + '/**/*.{jpg,png}';
+            // update the savePath variable to organize them in the same folder structure
+        }
+
+        console.log(this.arrayofFiles);
+    }
+
+    optimizeImages() {
+        imagemin(this.arrayofFiles, this.savePath, {
+            plugins: [
+                imageminPngquant({quality: '65-80'})
+            ]
+        }).then(files => {
+            //console.log(files);
+        });
+    }
+};
