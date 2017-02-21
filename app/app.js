05790fbbcb13d219ec869137339ce8777873174b
webtoast <webtoast@gmail.com>
2017-02-21 15:50:18 -0600

    replace dynamic text with static text

--- app/app.js
+++ app/app.js
@@ -5,7 +5,6 @@
 import os from 'os'; // native node.js module
 import { remote } from 'electron'; // native electron module
 import jetpack from 'fs-jetpack'; // module loaded from npm
-import { greet } from './hello_world/hello_world'; // code authored by you in this project
 import env from './env';
 import Files from './modules/files';
 
@@ -17,12 +16,7 @@ var appDir = jetpack.cwd(app.getAppPath());
 
 // This is the browser window with HTML and stuff, but you can read
 // files like it is node.js!
-// console.log('The author of this app is:', appDir.read('package.json', 'json').author);
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('greet').innerHTML = greet();
-});
-
+// console.log('The author of this app is:', appDir.read('package.json', 'json').author)
 document.ondragover = document.ondrop = (ev) => {
     ev.preventDefault();
 }
