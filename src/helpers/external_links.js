1879fe9d3cfe8e3eba4435d5734f623e99078651
webtoast <webtoast@gmail.com>
2018-03-18 22:22:50 -0500

    update helpers modules

--- src/helpers/external_links.js
+++ src/helpers/external_links.js
@@ -13,32 +13,28 @@
 //    <a href="http://bing.com">bing</a>
 // </p>
 
-(function () {
-    'use strict';
+import { shell } from "electron";
 
-    var shell = require('electron').shell;
+const supportExternalLinks = event => {
+  let href;
+  let isExternal = false;
 
-    var supportExternalLinks = function (e) {
-        var href;
-        var isExternal = false;
+  const checkDomElement = element => {
+    if (element.nodeName === "A") {
+      href = element.getAttribute("href");
+    }
+    if (element.classList.contains("js-external-link")) {
+      isExternal = true;
+    }
+    if (href && isExternal) {
+      shell.openExternal(href);
+      event.preventDefault();
+    } else if (element.parentElement) {
+      checkDomElement(element.parentElement);
+    }
+  };
 
-        var checkDomElement = function (element) {
-            if (element.nodeName === 'A') {
-                href = element.getAttribute('href');
-            }
-            if (element.classList.contains('js-external-link')) {
-                isExternal = true;
-            }
-            if (href && isExternal) {
-                shell.openExternal(href);
-                e.preventDefault();
-            } else if (element.parentElement) {
-                checkDomElement(element.parentElement);
-            }
-        };
+  checkDomElement(event.target);
+};
 
-        checkDomElement(e.target);
-    };
-
-    document.addEventListener('click', supportExternalLinks, false);
-}());
+document.addEventListener("click", supportExternalLinks, false);
