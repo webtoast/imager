70e496119fef7ef4d7c37e0937996f44428a8846
webtoast <webtoast@gmail.com>
2018-03-18 22:28:12 -0500

    update background.js

--- src/background.js
+++ src/background.js
@@ -1,42 +1,56 @@
 // This is main process of Electron, started as first thing when your
-// app starts. This script is running through entire life of your application.
+// app starts. It runs through entire life of your application.
 // It doesn't have any windows which you can see on screen, but we can open
 // window from here.
 
-import { app, Menu } from 'electron';
-import { devMenuTemplate } from './helpers/dev_menu_template';
-import { editMenuTemplate } from './helpers/edit_menu_template';
-import createWindow from './helpers/window';
+import path from "path";
+import url from "url";
+import { app, Menu } from "electron";
+import { devMenuTemplate } from "./menu/dev_menu_template";
+import { editMenuTemplate } from "./menu/edit_menu_template";
+import createWindow from "./helpers/window";
 
 // Special module holding environment variables which you declared
 // in config/env_xxx.json file.
-import env from './env';
+import env from "env";
 
-var mainWindow;
-
-var setApplicationMenu = function () {
-    var menus = [editMenuTemplate];
-    if (env.name !== 'production') {
-        menus.push(devMenuTemplate);
-    }
-    Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
+const setApplicationMenu = () => {
+  const menus = [editMenuTemplate];
+  if (env.name !== "production") {
+    menus.push(devMenuTemplate);
+  }
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
 };
 
-app.on('ready', function () {
-    setApplicationMenu();
+// Save userData in separate folders for each environment.
+// Thanks to this you can use production and development versions of the app
+// on same machine like those are two separate apps.
+if (env.name !== "production") {
+  const userDataPath = app.getPath("userData");
+  app.setPath("userData", `${userDataPath} (${env.name})`);
+}
 
-    var mainWindow = createWindow('main', {
-        width: 1000,
-        height: 600
-    });
+app.on("ready", () => {
+  setApplicationMenu();
 
-    mainWindow.loadURL('file://' + __dirname + '/app.html');
+  const mainWindow = createWindow("main", {
+    width: 1000,
+    height: 600
+  });
 
-    if (env.name !== 'production') {
-        mainWindow.openDevTools();
-    }
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "app.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  if (env.name === "development") {
+    mainWindow.openDevTools();
+  }
 });
 
-app.on('window-all-closed', function () {
-    app.quit();
+app.on("window-all-closed", () => {
+  app.quit();
 });
